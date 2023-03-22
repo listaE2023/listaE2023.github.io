5c6afff82a38cde59ffcf04fe6c5d2e263e44a6b
Miguel Freitas <radiodomiguel70@gmail.com>
2023-03-22 15:59:02 +0000

    Add deployment worflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://listae2023.github.io/',
+  base: '/istaE2023.github.io',
+});
