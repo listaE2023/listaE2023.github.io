42d6dbf381e5af3cfca8ce70ff7dc0b5ccddfbbd
Miguel Freitas <radiodomiguel70@gmail.com>
2023-04-30 23:59:44 +0100

    try config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,4 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://listae2023.github.io/',
-  base: '/listaE2023.github.io',
 });
